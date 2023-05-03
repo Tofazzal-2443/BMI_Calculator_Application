4d285445e04a8c2b8591cd569b4082b3d690c27e
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-03 15:20:37 +0600

    result page provider add

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,6 +1,8 @@
 import 'package:bmi_calculator/models/bmi_data_generator.dart';
+import 'package:bmi_calculator/provider/bmi_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:provider/provider.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -8,9 +10,12 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<BmiProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Result"),
+        backgroundColor: Colors.deepOrange,
+        title: Text("Result", style: TextStyle(fontSize: 30),),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -27,10 +32,10 @@ class ResultPage extends StatelessWidget {
                 radius: 90.0,
                 lineWidth: 25.0,
                 animation: true,
-                percent: 0.7,
+                percent: (provider.bmi /100),
                 progressColor: Colors.deepOrange,
                 center: new Text(
-                  "70.0 kg/m²",
+                  "${provider.bmi.toStringAsFixed(2)} kg/m²",
                   style: new TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20.0,
@@ -48,10 +53,10 @@ class ResultPage extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    "Normal",
+                    "${BmiDataGenerator.generateStatus(provider.bmi)}",
                     style: TextStyle(
                       fontSize: 25,
-                      color: Colors.blue,
+                      color: Colors.deepOrange,
                     ),
                   ),
                 ],
@@ -66,14 +71,14 @@ class ResultPage extends StatelessWidget {
                     "Category",
                     style: TextStyle(
                       fontSize: 25,
-                      color: Colors.blue,
+                      color: Colors.deepOrange,
                     ),
                   ),
                   Text(
                     "(kg/m²)",
                     style: TextStyle(
                       fontSize: 25,
-                      color: Colors.blue,
+                      color: Colors.deepOrange,
                     ),
                   ),
                 ],
@@ -86,7 +91,14 @@ class ResultPage extends StatelessWidget {
                   trailing: Text(
                       BmiDataGenerator.bmiTableMap.values.toList()[index]
                   ),
-
+                  tileColor: BmiDataGenerator.generateStatus(provider.bmi) ==
+                      BmiDataGenerator.bmiTableMap.keys.toList()[index]
+                      ? Colors.deepOrange
+                      : null,
+                  textColor: BmiDataGenerator.generateStatus(provider.bmi) ==
+                      BmiDataGenerator.bmiTableMap.keys.toList()[index]
+                      ? Colors.white
+                      : null,
                 ),
               ),
             ],
