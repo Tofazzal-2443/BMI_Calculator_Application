7016dae99b231d664217fa4ad8954909cdc3ca3c
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-03 13:54:36 +0600

    bmi calculation function add

--- lib/provider/bmi_provider.dart
+++ lib/provider/bmi_provider.dart
@@ -5,4 +5,21 @@ class BmiProvider extends ChangeNotifier{
   TextEditingController feetController = TextEditingController();
   TextEditingController inchController = TextEditingController();
 
+  clearField(){
+    weightController.clear();
+    feetController.clear();
+    inchController.clear();
+    notifyListeners();
+  }
+
+  double bmi = 0.0;
+
+  bmiCalculation(){
+    var weight = double.parse(weightController.text);
+    var feet = double.parse(feetController.text);
+    var inch = double.parse(inchController.text);
+    var meter = (feet * 0.3048) + (inch * 0.0254);
+    bmi = (weight) / (meter * meter);
+    notifyListeners();
+  }
 }
